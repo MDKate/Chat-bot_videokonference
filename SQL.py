a84d83bdd07b5f9facde550c6399af55f38be69c
DESKTOP-MHC35ME\50AdmNsk <medngueu@gmail.com>
2023-12-05 18:39:15 +0700

    Добавлен основной функционал и настроена пересылка сообщений

---
+++ SQL.py
@@ -0,0 +1,120 @@
+import os.path
+import sqlite3 as sq
+import pandas as pd
+
+async def db_start(): #Создание БД
+    global db, cur
+    db = sq.connect('videoconf.db')
+    cur = db.cursor()
+    table_name = 'Requests'
+    # sheet_name = "RequestsVC"
+    cur.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table_name}'")
+    result = cur.fetchone()
+    if result is None:
+        data = pd.DataFrame([{'ID': '', 'Redact': '', 'User_id': '', 'Name': '', 'Service': '', 'Problem': '',
+                                        'Degree': '', 'Address': '', 'timeV': '', 'timeAlt': ''}])
+        # print(data)
+        data.to_sql(table_name, sq.connect('videoconf.db'), index=False)
+    db.commit()
+
+
+async def user_id_record_from_db(table_name_db, user_id): #Записываем параметр user_id
+    df = pd.read_sql(f"SELECT * FROM Requests", sq.connect('videoconf.db'))
+    max_z = 0
+    if df['ID'][0] != "":
+        max_z = int(max((df['ID'].values).astype(int))+1)
+        sql_update_query = f"""INSERT INTO {table_name_db}(ID, Redact, User_id, Name, Service, Problem, Degree, Address, timeV, timeAlt) 
+                                VALUES ('', '', '', '', '', '', '', '', '', '')"""
+        cur.execute(sql_update_query)
+    sql_update_query = f"""Update {table_name_db} as A  set User_id = {user_id} where User_id = "" """
+    cur.execute(sql_update_query)
+    sql_update_query = f"""Update {table_name_db} as A  set ID = {max_z} where ID = "" """
+    cur.execute(sql_update_query)
+    sql_update_query = f"""Update {table_name_db} as A  set Redact = 1 where Redact = "" """
+    cur.execute(sql_update_query)
+    db.commit()
+
+async def parametr_redact_from_db(table_name_db, user_id, parametr_name, parametr): #Редактируем таблицу
+    sql_update_query = f"""Update {table_name_db} as A  set {parametr_name} = "{parametr}"
+        where User_id = {user_id} and Redact = 1"""
+    cur.execute(sql_update_query)
+    db.commit()
+
+async def Redact_redact_from_db(table_name_db, user_id): #Меняем статус запроса
+    sql_update_query = f"""Update {table_name_db} as A  set Redact = 0
+        where User_id = {user_id} and Redact = 1"""
+    cur.execute(sql_update_query)
+    db.commit()
+
+async def datetime_fix_from_db(table_name_db, user_id, datetime_fix): #Записываем текущие дату и время
+    sql_update_query = f"""Update {table_name_db} as A  set timeAlt = '{datetime_fix}'
+        where User_id = {user_id} and Redact = 1"""
+    cur.execute(sql_update_query)
+    db.commit()
+
+# async def del_null_row_from_db(table_name): #Удаляем первую пустую строку
+#     sql_update_query = f"""DELETE top(1) FROM {table_name} """
+#     cur.execute(sql_update_query)
+#     db.commit()
+
+async def load_from_db(table_name):
+    df = pd.read_sql(f"SELECT * FROM Requests", sq.connect('videoconf.db'))
+    return df
+
+async def forward_message_from_db(user_id):
+    df = pd.read_sql(f"SELECT * FROM Requests where User_id = {user_id} and Redact = 1 ", sq.connect('videoconf.db'))
+    return df
+
+# async def user_id_from_db(table_name_db, user_id, user_phone): #Вносим номер телефона участника
+#     sql_update_query = f"""Update {table_name_db} as A  set user_ID = {user_id}
+#         where A.ID = (
+#             select ID
+#             from ID
+#             where user_phone = {user_phone})"""
+#     cur.execute(sql_update_query)
+#     db.commit()
+
+# async def help_from_db(table_name_db, help_request, user_id): #Вносим чат-айди
+#     sql_update_query = f"""Update {table_name_db} as A set help_request = {help_request}
+#     where  user_ID = {user_id}"""
+#     cur.execute(sql_update_query)
+#     db.commit()
+
+# async def table_help_insert_from_db(user_id, text_message): #Создаем пометку о необходимости помощи
+#     # cur.execute(f"SELECT MAX(ID_help) FROM Help")
+#     # result = cur.fetchone()
+#     # hID = result[0] if not result[0] is None else 0
+#     df = pd.read_sql(f"SELECT * FROM help",
+#                      sq.connect('appointment.db'))
+#     hID = len(df) if not len(df) is None else 0
+#     text_message = text_message.replace("'", "*")
+#     text_message = text_message.replace('"', "*")
+#     sql_update_query = f"""INSERT INTO Help(ID_help, user_ID, text_message)
+#                             VALUES ({int(hID)+1}, {user_id}, '{text_message}')"""
+#     cur.execute(sql_update_query)
+#     db.commit()
+#     return hID if int(hID) > 0 else 0
+
+
+# async def user_id_search_from_db(table_name_db, user_phone): #Поиск пользователя в БД
+#     # cursor = db.cursor()
+#     cur.execute(f"SELECT user_name "
+#                 f"FROM {table_name_db} JOIN ID ON {table_name_db}.ID = ID.ID "
+#                 f"WHERE user_phone = {user_phone}")
+#     result = cur.fetchone()
+#     # await result[0]
+#     return result[0] if result else None
+
+
+
+# async def search_from_db(parametr, table_name_db, user_ID): #Поиск любого параметра по таблице CBAppointment
+#     # cursor = db.cursor()
+#     cur.execute(f"SELECT {parametr} "
+#                 f"FROM {table_name_db} "
+#                 f"WHERE user_ID = {user_ID} ")
+#     result = cur.fetchone()
+#     # print(cur.execute)
+#     # await result[0]
+#     return result[0] if result else None
+
+
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
-import datetime
+from datetime import datetime
 import os
 import pathlib
-
+from SQL import db_start, user_id_record_from_db, parametr_redact_from_db, Redact_redact_from_db, load_from_db, \
+    datetime_fix_from_db, forward_message_from_db
 
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.dispatcher import FSMContext
@@ -12,8 +13,8 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 # from aiogram.dispatcher.filters import Command
 # from filters.chat_type import ChatTypeFilter
 
-
-
+# global table_name_db
+table_name_db = 'Requests'
 
 storage = MemoryStorage()
 botMes = Bot(open(os.path.abspath('token.txt')).read())
@@ -31,69 +32,102 @@ class Form(StatesGroup):
 
 
 
-async def on_startup(_):
-    #await db_start()
-    print(1)
+async def on_startup(table_name):
+    await db_start()
+    print('start')
 
 @bot.message_handler(commands=['start'])  # Начинаем работу
-async def start(message: types.message):
+async def start(message: types.message, table_name_db=table_name_db):
+    # keyboard = types.ReplyKeyboardRemove()
+    # if keyboard.remove_keyboard == True:
+    #     await message.delete_reply_markup()
+    # await message.reply('', reply_markup=keyboard)
+    await user_id_record_from_db(table_name_db, message.from_user.id)
     await Form.name.set()
     await botMes.send_message(message.from_user.id, "Добрый день! \nПожалуйста, введите ваше ФИО.")
 
 @bot.message_handler(state=Form.name)  # Обработка всех текстовых сообщений
-async def name_message(message: types.Message, state: FSMContext):
+async def name_message(message: types.Message, state: FSMContext, table_name_db=table_name_db):
 
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     markup.add("Iva", "MTS-Link")
-
+    await parametr_redact_from_db(table_name_db, message.from_user.id, parametr_name='Name', parametr=message.text)
     await Form.next()
     await botMes.send_message(message.from_user.id, "Выберите сервис", reply_markup=markup)
 
 
 @bot.message_handler(state=Form.service)  # Обработка всех текстовых сообщений
-async def service_message(message: types.Message, state: FSMContext):
+async def service_message(message: types.Message, state: FSMContext, table_name_db=table_name_db):
+    await parametr_redact_from_db(table_name_db, message.from_user.id, 'Service', message.text)
     await Form.next()
     await botMes.send_message(message.from_user.id, "Опишите проблему")
 
 
 @bot.message_handler(state=Form.problem)  # Обработка всех текстовых сообщений
-async def problem_message(message: types.Message, state: FSMContext):
+async def problem_message(message: types.Message, state: FSMContext, table_name_db=table_name_db):
+    await parametr_redact_from_db(table_name_db, message.from_user.id, 'Problem', message.text)
     await Form.next()
-    await botMes.send_message(message.from_user.id, "Введите адрес, где произошла ошибка и номер кабинета (например: Фрунзе 57/1, каб.11")
+    await botMes.send_message(message.from_user.id, "Введите адрес, где произошла ошибка и номер кабинета (например: Фрунзе 57/1, каб.11)")
 
 
 @bot.message_handler(state=Form.degree)  # Обработка всех текстовых сообщений
-async def address_message(message: types.Message, state: FSMContext):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+async def address_message(message: types.Message, state: FSMContext, table_name_db=table_name_db):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     markup.add("Общий", "Локальный")
-
+    await parametr_redact_from_db(table_name_db, message.from_user.id, 'Address', message.text)
     await Form.next()
     await botMes.send_message(message.from_user.id, "Оцените степень влияния сбоя", reply_markup=markup)
 
 @bot.message_handler(state=Form.address)  # Обработка всех текстовых сообщений
-async def address_message(message: types.Message, state: FSMContext):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+async def degree_message(message: types.Message, state: FSMContext, table_name_db=table_name_db):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     markup.add("Сейчас", "Ввести врмя вручную")
-
+    await parametr_redact_from_db(table_name_db, message.from_user.id, 'Degree', message.text)
     await Form.next()
     await botMes.send_message(message.from_user.id, "Выберите время возникновения ошибки", reply_markup=markup)
 
 @bot.message_handler(state=Form.timeV)
-async def now_message(message: types.Message, state: FSMContext):
+async def now_message_v(message: types.Message, state: FSMContext, table_name_db=table_name_db):
     if message.text == "Сейчас":
-        await Form.set()
+        await parametr_redact_from_db(table_name_db, message.from_user.id, 'timeV', message.text)
+        await datetime_fix_from_db(table_name_db, message.from_user.id, str(datetime.now()))
+        df = await forward_message_from_db(message.from_user.id)
+        messageT = f"Запрос № {df['ID'][0]} \n" \
+                  f"ФИО: {df['Name'][0]} \n" \
+                  f"Сервис: {df['Service'][0]} \n" \
+                  f"Проблема: {df['Problem'][0]} \n" \
+                  f"Адрес: {df['Address'][0]} \n" \
+                  f"Влияние: {df['Degree'][0]} \n" \
+                  f"Время: {df['timeAlt'][0]}"
+        await Redact_redact_from_db(table_name_db, message.from_user.id)
+        await state.finish()
+        await botMes.send_message(open(os.path.abspath('chat.txt')).read(), messageT)
         await botMes.send_message(message.from_user.id, "Спасибо! Информация о сбое направлена в техподдержкку. "
                                                         "\n Для повторной отправки сообщения нажмите /start")
     if message.text == "Ввести врмя вручную":
+        await parametr_redact_from_db(table_name_db, message.from_user.id, 'timeV', message.text)
         await Form.next()
         await botMes.send_message(message.from_user.id, "Введите время")
 
 
 @bot.message_handler(state=Form.timeAlt)
-async def now_message(message: types.Message, state: FSMContext):
-    await Form.set()
+async def now_message_alt(message: types.Message, state: FSMContext, table_name_db=table_name_db):
+    await parametr_redact_from_db(table_name_db, message.from_user.id, 'timeAlt', message.text)
+    df = await forward_message_from_db(message.from_user.id)
+    messageT = f"Запрос № {df['ID'][0]} \n" \
+               f"ФИО: {df['Name'][0]} \n" \
+               f"Сервис: {df['Service'][0]} \n" \
+               f"Проблема: {df['Problem'][0]} \n" \
+               f"Адрес: {df['Address'][0]} \n" \
+               f"Влияние: {df['Degree'][0]} \n" \
+               f"Время: {df['timeAlt'][0]}"
+    await Redact_redact_from_db(table_name_db, message.from_user.id)
+    await state.finish()
+    await botMes.send_message(open(os.path.abspath('chat.txt')).read(), messageT)
     await botMes.send_message(message.from_user.id, "Спасибо! Информация о сбое направлена в техподдержкку. "
                                                     "\n Для повторной отправки сообщения нажмите /start")
+
+
 
 
 if __name__ == '__main__':
